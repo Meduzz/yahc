87845888bccd503eade1b4d85d7d50ca1f680b29
Bjärn Bnegtsson <drgevalia@gmail.com>
2018-12-22 15:02:11 +0100

    Add a simple but competent way to sign request.

--- src/main/scala/se/kodiak/tools/yahc/Yahc.scala
+++ src/main/scala/se/kodiak/tools/yahc/Yahc.scala
@@ -5,36 +5,62 @@ import scalaj.http.{Http, HttpRequest, HttpResponse}
 
 object Yahc {
 
+	type Signer = (String, HttpRequest)=>HttpRequest
+	val NoBody = ""
+
 	object GET {
-		def apply(url:String):HttpRequest = request(url, "GET")
+		def apply(url:String, signer:Option[Signer] = None):HttpRequest = {
+			val req = request(url, "GET")
+
+			signer.map(sign => sign(NoBody, req))
+		  	.getOrElse(req)
+		}
 	}
 
 	object POST {
-		def apply(url:String, entity:AnyRef):HttpRequest = {
+		def apply(url:String, entity:AnyRef, signer:Option[Signer] = None):HttpRequest = {
 			import org.json4s.native.Serialization.write
 
-			request(url, "POST")
-				.postData(write(entity)(DefaultFormats))
+			val body = write(entity)(DefaultFormats)
+			val req = request(url, "POST")
+				.postData(body)
 		  	.header("Content-Type", "application/json")
+
+			signer.map(sign => sign(body, req))
+		  	.getOrElse(req)
 		}
 	}
 
 	object PUT {
-		def apply(url:String, entity:AnyRef):HttpRequest = {
+		def apply(url:String, entity:AnyRef, signer:Option[Signer] = None):HttpRequest = {
 			import org.json4s.native.Serialization.write
 
-			request(url, "PUT")
-				.put(write(entity)(DefaultFormats))
+			val body = write(entity)(DefaultFormats)
+			val req = request(url, "PUT")
+				.put(body)
 				.header("Content-Type", "application/json")
+
+			signer.map(sign => sign(body, req))
+		  	.getOrElse(req)
 		}
 	}
 
 	object DELETE {
-		def apply(url:String):HttpRequest = request(url, "DELETE")
+		def apply(url:String, signer:Option[Signer] = None):HttpRequest = {
+			val req = request(url, "DELETE")
+
+			signer.map(sign => sign(NoBody, req))
+		  	.getOrElse(req)
+		}
 	}
 
 	object HEAD {
-		def apply(url:String):HttpRequest = request(url, "HEAD")
+		def apply(url:String, signer:Option[Signer] = None):HttpRequest = {
+			val req = request(url, "HEAD")
+
+			signer.map(sign => sign(NoBody, req))
+				.getOrElse(req)
+		}
 	}
 
 	def request(url:String, method:String):HttpRequest = Http(url).method(method)
--- src/test/scala/se/kodiak/tools/yahc/Server.scala
+++ src/test/scala/se/kodiak/tools/yahc/Server.scala
@@ -6,7 +6,7 @@ import org.scalatest.{BeforeAndAfterAll, FunSuite}
 import se.chimps.cameltow.Cameltow
 import se.chimps.cameltow.framework.Encoded
 import se.chimps.cameltow.framework.handlers.Action
-import se.chimps.cameltow.framework.responsebuilders.{Error, Ok}
+import se.chimps.cameltow.framework.responsebuilders.{BadRequest, Error, Ok}
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
@@ -35,6 +35,27 @@ trait Server extends BeforeAndAfterAll { self:FunSuite =>
 	routes.PUT("/put/echo", Action(req => {
 		req.body match {
 			case Encoded(bytes) => bytes.map(Ok.json)
+			case _ => Future(Error.text("Not an encoded body."))
+		}
+	}))
+
+	routes.POST("/signed", Action(req => {
+		req.body match {
+			case Encoded(bytes) => {
+				bytes.map(bs => {
+					val len = bs.length
+					val sig = req.header("Signature") match {
+						case Some(s) => s.toInt
+						case None => 0
+					}
+
+					if (len == sig) {
+						Ok.json("{}")
+					} else {
+						BadRequest.json("{}")
+					}
+				})
+			}
 			case _ => Future(Error.text("Not an encoded body."))
 		}
 	}))
